42cce8a6ce74627beee36b4a92dc3c22131de615
Varun Sharma <varunpost@gmail.com>
2017-03-06 14:05:19 -0600

    Added some tests

--- tests/testfunc.js
+++ tests/testfunc.js
@@ -7,3 +7,4 @@ console.log(ccbooleananalysis.getParseTree("NOT"));
 console.log(ccbooleananalysis.getParseTree("RAND OR NAND or LCKP1"));
 console.log(ccbooleananalysis.getParseTree("OR"));
 console.log(ccbooleananalysis.getParseTree("NOT AB"));
+console.log(ccbooleananalysis.getParseTree("NO OR ROR"));
