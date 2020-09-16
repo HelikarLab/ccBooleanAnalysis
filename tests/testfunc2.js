5ead027b9dc4515b2df17d39702385aa23a40386
Ales Saska <mail@alessaska.cz>
2020-09-16 17:30:48 -0400

    build

---
+++ tests/testfunc2.js
@@ -0,0 +1,8 @@
+var ccbooleananalysis = require("../src/ccBooleanAnalysis.js");
+
+
+var eq1 = "0";
+var eq2 = "( ( ( S_4  *  ( ( ( S_75   *  S_2 ) ) )     )  *  ~  ( S_115  )  )  *  ~  ( S_2  ) )";
+
+console.log(JSON.stringify(ccbooleananalysis.compareBooleansSAT(eq1, eq2)));
+
